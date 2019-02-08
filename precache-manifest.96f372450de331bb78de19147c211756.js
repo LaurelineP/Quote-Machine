20363ece4b94f276540d6e285591b358b3b201d6
Laureline <laurelinedev@gmail.com>
2019-02-08 01:25:25 +0100

    Updates

---
+++ precache-manifest.96f372450de331bb78de19147c211756.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7310f8430c37539d9d8b",
+    "url": "/Quote-Machine/static/js/runtime~main.7310f843.js"
+  },
+  {
+    "revision": "c13ee929d295f52c48b7",
+    "url": "/Quote-Machine/static/js/main.c13ee929.chunk.js"
+  },
+  {
+    "revision": "babd94826ba891d51a02",
+    "url": "/Quote-Machine/static/js/1.babd9482.chunk.js"
+  },
+  {
+    "revision": "c13ee929d295f52c48b7",
+    "url": "/Quote-Machine/static/css/main.4b1c1624.chunk.css"
+  },
+  {
+    "revision": "2d1fb6ec9d277a36fcf223fc6a917f7e",
+    "url": "/Quote-Machine/index.html"
+  }
+];
